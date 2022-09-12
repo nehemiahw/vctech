19887e75b7a169e7fc9eaeb7d1ee7e815d95123d
nehemiahw <nehemiah.washington6@gmail.com>
2022-09-11 22:24:34 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from '../src/partials/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/partials/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar(){
+
+return(
+    <div className="w3-container">
+        <h1>Home</h1>
+    </div>
+);
+
+}
+
+export default Navbar;
